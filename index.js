0440782340250ed757d4cf6086540f6179ad3033
shreykapadia <shreykapadia@gmail.com>
2017-09-25 01:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much."
+function kittens(){
+  var kittens = ["Milo", "Otis", "Garfield"]
+}
+function destructivelyAppendKitten(name){
+  name.push("Ralph");
+  return name
+}
